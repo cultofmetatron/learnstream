4c5b971f63609e0614eea6aae1e10cfcd4df3de2
YoungNeem <nima.mehanian@gmail.com>
2013-05-04 14:57:38 -0700

    Edit whitespacing in helpers.js

--- server/helpers.js
+++ server/helpers.js
@@ -1,12 +1,9 @@
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-
-    next();
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
 };
-
-
 
 module.exports = {};
 module.exports.allowCrossDomain = allowCrossDomain;
