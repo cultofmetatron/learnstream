d90937d52c2502c3b7e77feb10517c172fb32827
YoungNeem <nima.mehanian@gmail.com>
2013-05-04 20:37:52 -0700

    Reorder script tags to load TextBoxList. Add styling for list items

--- public/assets/javascripts/tbListConfig.js
+++ public/assets/javascripts/tbListConfig.js
@@ -1,4 +1,7 @@
-document.ready(function() {
+$(document).ready(function() {
   var t = new $.TextboxList('#helpTags');
-  t.add('Tag 1').add('Tag 2').add('Tag 3');
+  t.add('jQuery').add('JavaScript').add('AJAX');
+
+  var t2 = new $.TextboxList('#helpTagsMentor');
+  t2.add('jQuery').add('JavaScript').add('AJAX');
 });
