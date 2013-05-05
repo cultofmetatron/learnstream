f6d3bb7fb53f6ce11243f0bbe527077066b28457
YoungNeem <nima.mehanian@gmail.com>
2013-05-05 10:57:49 -0700

    Indentation organization

--- public/assets/javascripts/questionstream.js
+++ public/assets/javascripts/questionstream.js
@@ -1,15 +1,14 @@
-//load the stream of questions
+// Load the stream of questions
 (function() {
-  //rooms models
+  // Rooms models
   window.Question = Backbone.Model.extend({
     defaults: function() {
       return {
         desc: 'Teach me something!'
-        //solved: false
+        // solved: false
       };
     },
     initialize: function() {
-      //initialization code
       if (!this.get('desc')) {
         this.set({'title':this.defaults().title});
       }
@@ -18,8 +17,7 @@
 
   window.Tag = Backbone.Model.extend({
     defaults: function() {
-      return {
-      };
+      return {};
     }
   });
 
@@ -32,45 +30,42 @@
     window.QuestionView = Backbone.View.extend({
     tagName: 'li',
     template: Handlebars.compile($('#question-item').html().trim()),
-    events: {
-      },
-
+    events: {},
     initialize: function() {
       this.listenTo(this.model, 'change', this.render);
       this.listenTo(this.model, 'remove', this.remove);
-      },
-
+    },
     render: function() {
       this.$el.attr('id', this.model.id);
       return this.$el.html(this.template(this.model.toJSON()));
     }
   });
 
-    window.QuestionsView = Backbone.View.extend({
-      tagName: 'ul',
-      collection: Questions,
-      render: function() {
-        var html = [];
-        $(this.collection.each(function(question) {
-          var questionView = new QuestionView({
-            model: question
-          });
-          questionView.render();
-          html.push(questionView.el);
-        }));
-        this.$el.html(html);
-      }
-    });
+  window.QuestionsView = Backbone.View.extend({
+    tagName: 'ul',
+    collection: Questions,
+    render: function() {
+      var html = [];
+      $(this.collection.each(function(question) {
+        var questionView = new QuestionView({
+          model: question
+        });
+        questionView.render();
+        html.push(questionView.el);
+      }));
+      this.$el.html(html);
+    }
+  });
 
-    var questions = new Questions();
-      window.questionsView = new QuestionsView({collection: questions});
-      questionsView.render();
+  var questions = new Questions();
+    window.questionsView = new QuestionsView({collection: questions});
+    questionsView.render();
 
-      var reloadTasks = function() {
+    var reloadTasks = function() {
       //questions.fetch();
       questions.fetch();
       questionsView.render();
-      console.log( $('#question-item'));
+      console.log($('#question-item'));
       $('#question-item').append(questionsView.el);
       setInterval(reloadTasks, 5000);
     };
